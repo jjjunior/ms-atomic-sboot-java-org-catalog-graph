1fcd17ceb94fe98ffd01e6ffc57c41b4f6a0fd5b
João José Maranhão Junior <joao.junior@ensino.ipt.br>
2025-09-29 22:07:00 -0300

    feat: implement list and update methods in TenantRestController for tenant management

--- src/main/java/br/com/jstack/org/catalog/graph/framework/adapter/input/rest/TenantRestController.java
+++ src/main/java/br/com/jstack/org/catalog/graph/framework/adapter/input/rest/TenantRestController.java
@@ -1,5 +1,7 @@
 package br.com.jstack.org.catalog.graph.framework.adapter.input.rest;
 
+import java.util.List;
+
 import br.com.jstack.org.catalog.graph.api.TenantsApi;
 import br.com.jstack.org.catalog.graph.application.port.input.shared.CreateUseCase;
 import br.com.jstack.org.catalog.graph.application.port.input.shared.DeleteByIdUseCase;
@@ -48,12 +50,21 @@ public class TenantRestController implements TenantsApi {
 	
 	@Override
 	public ResponseEntity<PagedListResponse> listTenants(String q, Integer page, Integer size) {
-		return ResponseEntity.status(501).build(); // Not Implemented
+		List<Tenant> tenants = retrieveAllUseCase.retrieveAll();
+//		List<Tenant> responses = businessDomains.stream()
+//			.map(mapper::domainToResponse)
+//			.toList();
+		PagedListResponse response = new PagedListResponse();
+		return ResponseEntity.ok(response);
 	}
 	
 	@Override
 	public ResponseEntity<ItemResponse> updateTenant(String tenantId, TenantRequest tenantRequest) {
-		return ResponseEntity.status(501).build(); // Not Implemented
+		Tenant       tenant   = mapper.requestToDomain(tenantRequest);
+//		tenant.setTenantId(tenantId);
+		Tenant                updated  = updateUseCase.update(tenant);
+		ItemResponse response = mapper.domainToResponse(updated);
+		return ResponseEntity.ok(response);
 	}
 }
 
